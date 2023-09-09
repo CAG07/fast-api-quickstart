11a0a863c4f38015d1787ea32b2d03f22eb84a88
CAG07 <craig.gehrig@gmail.com>
2023-09-09 07:19:21 -0500

    Create dockerfile

--- test_app.py
+++ test_app.py
@@ -56,4 +56,27 @@ def test_get_courses_filter_by_domain_and_sort_by_date():
     assert len(courses) > 0
     assert all([c['domain'][0] == 'mathematics' for c in courses])
     assert sorted(courses, key=lambda x: x['date'], reverse=True) == courses
-    
+
+def test_rate_chapter():
+    course_id = "64f47e1e0aa503e60c129ae3"
+    chapter_id = "1"
+    rating = 1
+
+    response = client.post(f"/courses/{course_id}/{chapter_id}?rating={rating}")
+
+    assert response.status_code == 200
+
+    # Check if the response body has the expected structure
+    assert "name" in response.json()
+    assert "rating" in response.json()
+    assert "total" in response.json()["rating"]
+    assert "count" in response.json()["rating"]
+
+    assert response.json()["rating"]["total"] > 0
+    assert response.json()["rating"]["count"] > 0
+
+
+def test_rate_chapter_not_exists():
+    response = client.post("/courses/64f47e1e0aa503e60c129ae3/990/rate", json={"rating": 1})
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Not Found'}
